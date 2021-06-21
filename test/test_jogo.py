35a46f6d270906ba0b7e9e16b18d51f49b9de1a6
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-06-21 08:28:36 +0100

    em desenvolvimento

--- test/test_jogo.py
+++ test/test_jogo.py
@@ -1,6 +1,6 @@
-from typing import List
 from unittest import TestCase
 from ap_qt import J3A7P6
+import string
 
 class jogo(TestCase):
 
@@ -10,3 +10,32 @@ class jogo(TestCase):
     def test_verifi_palavra(self):
         self.assertTrue(self.app.palavras)
         print(self.app.palavras)
+
+    def test_historico(self):
+        db = open("historico.txt","a+")
+        with db:
+            n=100
+            eng2sp="{'pontos':100},"
+            db.write(eng2sp)
+            db.close()
+        self.assertTrue(db)
+
+    def test_read_historico(self):
+        def process_file(filename):
+            hist = dict()
+            fp = open(filename)
+            for line in fp:
+                process_line(line, hist)
+            
+            print(hist)
+
+        def process_line(line, hist):
+            line = line.replace('-', ' ')
+            for word in line.split():
+                word = word.strip(string.punctuation + string.whitespace)
+                word = word.lower()
+                hist[word] = hist.get(word)
+                
+                
+        hist = process_file('historico.txt')
+        self.assertIsNone(hist)
