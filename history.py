f2b2e8f03f1bfdbc1a078e95357f39044014b709
Nurul-GC <nuruldecarvalho@gmail.com>
2021-07-17 19:51:27 +0200

    atualizando o history.py

--- history.py
+++ history.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def connection():
+def connect_db():
     """
     criando conexão com db sqlite
     """
@@ -14,17 +14,14 @@ def connection():
         raise erro
 
 
-def tabela_jogo():
-    db = connection()
-    query = """
-    CREATE TABLE IF NOT EXISTS tb_jogo(
-     id integer primary key autoincrement,
-     nome varchar(50) not null,
-     pontos varchar(20) not null,
-     jogada varchar(10) not null,
-     nivel varchar(5) not null
-    );
-     """
+def criar_tabela_jogo():
+    db = connect_db()
+    query = "CREATE TABLE IF NOT EXISTS tb_jogo" \
+            "(id integer primary key autoincrement," \
+            " nome varchar(50) not null," \
+            " pontos varchar(20) not null," \
+            " jogada varchar(10) not null," \
+            " nivel varchar(5) not null);"
     try:
         sql = db.cursor()
         result = sql.execute(query)
@@ -35,7 +32,7 @@ def tabela_jogo():
         raise erro
 
 
-def add_pontos(_nome, _pontos, _jogada, _nivel):
+def add_dados(_nome, _pontos, _jogada, _nivel):
     """
     criando historico de jogada
     : ** campos obrigatórios! **
@@ -44,8 +41,10 @@ def add_pontos(_nome, _pontos, _jogada, _nivel):
     - jogada
     - nivel
     """
-    db = connection()
-    query = f'INSERT INTO tb_jogo(nome,pontos,jogada,nivel)VALUES("{_nome}","{_pontos}","{_jogada}","{_nivel}");'
+    db = connect_db()
+    query = f'INSERT INTO tb_jogo' \
+            f'(nome,pontos,jogada,nivel)' \
+            f'VALUES("{_nome}","{_pontos}","{_jogada}","{_nivel}");'
 
     try:
         sql = db.cursor()
@@ -57,8 +56,8 @@ def add_pontos(_nome, _pontos, _jogada, _nivel):
         raise er
 
 
-def view_pontos():
-    db = connection()
+def ver_dados():
+    db = connect_db()
     query = f"SELECT nome,pontos,jogada,nivel FROM tb_jogo;"
     try:
         sql = db.cursor()
@@ -72,7 +71,7 @@ def view_pontos():
 
 
 def apagar_historico():
-    db = connection()
+    db = connect_db()
     query = "DELETE FROM tb_jogo;"
     try:
         sql = db.cursor()
