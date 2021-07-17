3e82655e9747b25a800a1192062d1ef509061edb
Nurul-GC <nuruldecarvalho@gmail.com>
2021-07-17 19:40:25 +0200

    atualizando o history.py

--- history.py
+++ history.py
@@ -1,15 +1,15 @@
 import sqlite3
-from sqlite3 import Error
-from sqlite3.dbapi2 import IntegrityError
+
 
 def connection():
     """
     criando conexão com db sqlite
     """
     try:
-        return sqlite3.connect("historico.db")
-    except Error as er:
-        raise er
+        return sqlite3.connect("historico/ap.db")
+    except sqlite3.Error as erro:
+        raise erro
+
 
 def tabela_jogo():
     db = connection()
@@ -17,74 +17,65 @@ def tabela_jogo():
     CREATE TABLE IF NOT EXISTS tb_jogo(
      id integer primary key autoincrement,
      nome varchar(50) not null,
-     pontos varchar(9) not null,
+     pontos varchar(20) not null,
      jogada varchar(10) not null,
      nivel varchar(5) not null
     );
      """
     try:
-        sql=db.cursor()
+        sql = db.cursor()
         result = sql.execute(query)
         db.commit()
         return result
-    except Error as er:
+    except sqlite3.Error as erro:
         db.rollback()
-        raise er
-    
+        raise erro
 
-def add_pontos(*args):
+
+def add_pontos(_nome, _pontos, _jogada, _nivel):
     """
     criando historico de jogada
     : ** campos obrigatórios! **
     - nome
     - pontos
     - jogada
-    - nivel 
-    
-
+    - nivel
     """
     db = connection()
-    #return f""" "{args[0]}","{args[1]}" """
-    query =f"""
-        INSERT INTO tb_jogo(nome,pontos,jogada,nivel)VALUES("{args[0]}","{args[1]}","{args[2]}","{args[3]}");
-        """
+    query = f'INSERT INTO tb_jogo(nome,pontos,jogada,nivel)VALUES("{_nome}","{_pontos}","{_jogada}","{_nivel}");'
 
     try:
-        sql=db.cursor()
+        sql = db.cursor()
         result = sql.execute(query)
         db.commit()
         return result
-    except Error as er:
+    except sqlite3.Error as er:
         db.rollback()
         raise er
-        
 
 
 def view_pontos():
     db = connection()
-    query =f"""
-        SELECT nome,pontos,jogada,nivel FROM tb_jogo;
-        """
+    query = f"SELECT nome,pontos,jogada,nivel FROM tb_jogo;"
     try:
-        sql=db.cursor()
+        sql = db.cursor()
         sql.execute(query)
         result = sql.fetchall()
         db.commit()
         return result
-    except Error as er:
-        db.rollback
-        raise er
+    except sqlite3.Error as erro:
+        db.rollback()
+        raise erro
+
 
 def apagar_historico():
     db = connection()
-    query ="""
-        DELETE FROM tb_jogo;
-        """
+    query = "DELETE FROM tb_jogo;"
     try:
-        sql=db.cursor()
-        result=sql.execute(query)
+        sql = db.cursor()
+        result = sql.execute(query)
         db.commit()
         return result
-    except Error as er:
-        db.rollback
-        raise er
+    except sqlite3.Error as erro:
+        db.rollback()
+        raise erro
