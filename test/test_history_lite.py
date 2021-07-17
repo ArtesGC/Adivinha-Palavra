96c7b3c7c6cabd85165689f8115820163b07f7b6
Nurul-GC <nuruldecarvalho@gmail.com>
2021-07-17 19:52:17 +0200

    atualizando os testes

--- test/test_history_lite.py
+++ test/test_history_lite.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from history import connection,tabela_jogo,add_pontos,view_pontos,apagar_historico
+from history import connect_db,criar_tabela_jogo,add_dados,ver_dados,apagar_historico
 
 class jogo(TestCase):
 
@@ -7,21 +7,21 @@ class jogo(TestCase):
         pass
 
     def test_connect(self):
-        conection = connection()
+        conection = connect_db()
         self.assertTrue(True)
     
     def test_create_table(self):
-        criar_tabela_historico = tabela_jogo()
+        criar_tabela_historico = criar_tabela_jogo()
         print(criar_tabela_historico)
         self.assertTrue(criar_tabela_historico)
 
     def test_criando_dados(self):
-        criar_historico = add_pontos("Lopes",100,10,1)
+        criar_historico = add_dados("Lopes", 100, 10, 1)
         print(criar_historico)
         self.assertTrue(criar_historico)
 
     def test_ver_dados_in_player(self):
-        ver_historico = view_pontos()
+        ver_historico = ver_dados()
         for vl in ver_historico:
             print(vl)
         self.assertTrue(ver_historico)
